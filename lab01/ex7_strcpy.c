37c7b7767dc41069cf55d433d552409db88a3f73
JWang <992961659@qq.com>
2024-08-16 01:38:58 +0800

    aug 16 update

--- lab01/ex7_strcpy.c
+++ lab01/ex7_strcpy.c
@@ -5,13 +5,13 @@
 int main() {
   // TODO: Allocate memory to store the string "hello"
   // You may use your solution from a previous exercise;
-  ______ message[______];
-  ______ = 'h';
-  ______ = 'e';
-  ______ = 'l';
-  ______ = 'l';
-  ______ = 'o';
-  ______ = ______;
+  char message[6];
+  message[0] = 'h';
+  message[1] = 'e';
+  message[2] = 'l';
+  message[3] = 'l';
+  message[4] = 'o';
+  message[5] = '\0';
 
   // Print out the value before we change message
   printf("Before copying: %s\n", message);
@@ -19,9 +19,8 @@ int main() {
   // Creates another_string that contains a longer string
   char* long_message = "Here's a really long string";
 
-  // TODO: Copy the string in long_message to message
-  // NOTE: This file is supposed to crash when you run it. You will be fixing this in another file.
-  strcpy(______, ______);
+  // TODO: Copy the string in long_message to message using strncpy
+  strcpy(message + 5, long_message);
 
   // Print out the value after we change message
   printf("After copying: %s\n", message);
